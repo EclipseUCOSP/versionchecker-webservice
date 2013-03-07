7c1b9502ff7f5204030ec046283b525c428be530
Chris Conley <chrisconley15@gmail.com>
2013-03-06 23:31:55 -0500

    updated model & controller for db efficiency
    Updated database connections and cut down on variables passing between
    functions. moved a lot of things from the controller to the model to
    make all the work done in the model.

--- cakephp-2.2.7/app/Controller/EclipseController.php
+++ cakephp-2.2.7/app/Controller/EclipseController.php
@@ -1,33 +1,19 @@
-
 <?php
 class EclipseController extends AppController{
-	
-	protected static $_messages = array(
-		JSON_ERROR_STATE_MISMATCH => 'Invalid or malformed JSON',
-		JSON_ERROR_SYNTAX => 'Syntax error',
-		JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded'
-         );
-	
-	
-	
+
+	#protected static $_messages = array(
+	#	JSON_ERROR_STATE_MISMATCH => 'Invalid or malformed JSON',
+		#JSON_ERROR_SYNTAX => 'Syntax error',
+		#JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded'
+      #   );
+
+
+
 	public function recievePOST(){
 		$array = array();
 		if ($this->request->isPost()){
-			# echo "its a post request!\n";
-			$array = array();
-			# recieve 
-			$JSON = $this->request->input('json_decode', true);
-			# open db here so we only have one instance open
-			$con = mysql_connect("localhost:3306","root","root");
-			foreach( $JSON as $obj ){
-				# test prints
-				# echo $obj['name'];
-				# echo "\n";
-				# echo $obj['version'];
-				$array[] = $this->Eclipse->versioncheck($obj['name'],$obj['version'],$con);
-				#$array[] = $sendJSON;
-			}
-			mysql_close($con);
+			$JSON[] = $this->request->input('json_decode', true);
+			$array[] = $this->Eclipse->versionchecker($JSON);
 		}
 		$this->set('sendJSON', $array);
 		$this->autoLayout = false;
@@ -35,5 +21,3 @@ class EclipseController extends AppController{
 	}
 }
 ?>
-
-
--- cakephp-2.2.7/app/Model/Eclipse.php
+++ cakephp-2.2.7/app/Model/Eclipse.php
@@ -1,36 +1,47 @@
-
 <?php
 
 class Eclipse extends Model {
 
 
-	public function versioncheck($id,$version,$con){
+	public function versionchecker(array $JSON){
 		# echo "got to model";
-		mysql_select_db("eclipsedb", $con) or die ("Connection Error");
+		$database='';
+		$hostname='';
+		$username='';
+		$password='';
+		$link = mysql_connect($hostname, $username, $password)
+									or die ("Database Connection Error");
+		mysql_select_db($database, $link) or die ("Connection Error");
 		#improve this for real db
-		$entry = mysql_query("SELECT * FROM version");
-		while($row = mysql_fetch_array($entry)){	
- 			if(strcmp($row['id'],$id)==0){
- 				#testing prints ignore for now
- 				#echo $id;
- 				#echo "\n";
-				#echo $row['id'];
-				#up to date
-				if (strcmp($row['p2_version'],$version)==0){
-					$results=array("component"=> $id,
+		$results = array();
+		foreach($JSON as $obj){
+			$id = $obj['name'];
+			$version = $obj['version'];
+			#$query = sprintf("SELECT * FROM version WHERE id='%s'",
+			#						mysql_real_escape_string($obj['name']));
+			$entry = mysql_query("SELECT * FROM version");
+			while($row = mysql_fetch_array($entry)){
+ 			if(strcmp($row['id'],$obj['name'])==0){
+				if (strcmp($row['p2_version'],$obj['version'])==0){
+					$results=array("component"=> $obj['name'],
 									"current"=> true
 									);
 				}else{ 
-				# not up to dat
-					$results=array("component"=> $id,
-									"current"=> array("repo" => $row['git_repo'],
-							 		 			 "commit" => $row['git_commit'],												  		"branch" => $row['git_branch']
+					# not up to dat
+					$results=array( "component"=> $obj['name'],
+									"current"=> array(
+													"repo" => $row['git_repo'],
+			 					  					"commit" => $row['git_commit'],
+								   					"branch" => $row['git_branch'],
+								 					"LTS" => $row['LTS']
 													)
-									);
+								);
 					}
 				}
 			}
-			return $results;
 		}
+		mysql_close($link);
+		return $results;
+		}			
 	}
 ?>
