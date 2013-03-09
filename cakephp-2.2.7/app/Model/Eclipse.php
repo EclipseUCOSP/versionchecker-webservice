61fa307675c81986a4324d61cb033ca51f31c742
Chris Conley <chrisconley15@gmail.com>
2013-03-08 23:16:19 -0500

    updated JSON output to reflect component availability
    Changed the JSON output to work with the "state" of the componenet
    (available, unavilable, alternative) instead of by version number.

--- cakephp-2.2.7/app/Model/Eclipse.php
+++ cakephp-2.2.7/app/Model/Eclipse.php
@@ -4,40 +4,45 @@ class Eclipse extends Model {
 
 
 	public function versionchecker(array $JSON){
-		# echo "got to model";
-		#$database='';
-		#$hostname='';
-		#$username='';
-		#$password='';
-		include('../Config/dbinfo.php');
+		
+		#database credentials
+		include('app/Config/dbinfo.php');
 		$link = mysql_connect($hostname, $username, $password)
 									or die ("Database Connection Error");
 		mysql_select_db($database, $link) or die ("Connection Error");
+		#remove db credentials from memory
 		unset($database,$hostname,$username,$password);
-		#improve this for real db
 		$results = array();
 		foreach($JSON as $obj){
-			$id = $obj['name'];
+			$id = $obj['component'];
 			$version = $obj['version'];
-			#$query = sprintf("SELECT * FROM version WHERE id='%s'",
-			#						mysql_real_escape_string($obj['name']));
 			$entry = mysql_query("SELECT * FROM version");
 			while($row = mysql_fetch_array($entry)){
- 			if(strcmp($row['id'],$obj['name'])==0){
-				if (strcmp($row['p2_version'],$obj['version'])==0){
-					$results=array("component"=> $obj['name'],
-									"current"=> true
-									);
-				}else{ 
-					# not up to dat
-					$results=array( "component"=> $obj['name'],
-									"current"=> array(
-													"repo" => $row['git_repo'],
-			 					  					"commit" => $row['git_commit'],
-								   					"branch" => $row['git_branch'],
-								 					"LTS" => $row['LTS']
-													)
-								);
+ 				if(strcmp($row['id'],$id)==0){
+ 					#part up to date
+					#if (strcmp($row['p2_version'],$id)==0){
+					#	$results=array("component"=> $obj['name'],
+					#					"current"=> true
+					#					);
+					#}else{ 
+						# not up to date
+						#component is unavailable
+						if (strcmp($row['LTS'],"unavailable")==0){
+							$results=array( "component"=> $id,
+											"state"=>"unavailable"
+											);   
+						}else{
+							#component is available or has alternative source
+							# LTS support not implemented
+							$results=array("component"=> $id,
+						       				"state" => $row['LTS'],
+						       				"version"=> $row['p2_version'],
+											"repo"=> array(
+														"repoinfo" => $row['git_repo'],
+	 													"commit" => $row['git_commit'],
+														"branch" => $row['git_branch']
+															)	
+											);
 					}
 				}
 			}
